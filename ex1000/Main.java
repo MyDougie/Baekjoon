93b31b6f13af9fd99e7708e7c36d1348d9c63c3f
MyDougie <01046009100@hanmail.net>
2017-06-13 22:18:52 +0900

    Impl

---
+++ ex1000/Main.java
@@ -0,0 +1,15 @@
+package ex1000;
+
+import java.util.Scanner;
+/**
+ * Impl - 2017/04/16 - Complete
+ * */
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println(a+b);
+
+	}
+}
