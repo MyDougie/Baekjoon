ddda406ddc69de17589f664c543466855027c8fa
ParkTaegyu <01046009100@hanmail.net>
2017-07-15 15:05:03 +0900

    그대로 출력하기

---
+++ ex11718/Main.java
@@ -0,0 +1,18 @@
+package ex11718;
+
+/**
+ * 그대로 출력하기 - 2017/04/21 - Complete
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNextLine()){
+			System.out.println(input.nextLine());
+		}
+		
+	}
+}
