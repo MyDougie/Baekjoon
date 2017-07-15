7d07d31c7905fb162a96af2ff4dd0bb0ee876fc1
ParkTaegyu <01046009100@hanmail.net>
2017-07-15 12:25:40 +0900

    입출력

---
+++ ex7287/Main.java
@@ -0,0 +1,14 @@
+package ex7287;
+
+/**
+ * 입출력 - 2017/07/15 - Complete
+ * */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(45);
+		System.out.println("qkrxorb10");
+		
+	}
+
+}
