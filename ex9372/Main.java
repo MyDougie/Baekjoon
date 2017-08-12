2cf6d2abdb58719075a0e86506d5226f3ea2c87a
ParkTaegyu <01046009100@hanmail.net>
2017-08-12 15:42:45 +0900

    최소 스패닝 트리(상근이의 여행)

---
+++ ex9372/Main.java
@@ -0,0 +1,30 @@
+package ex9372;
+/**
+ * 최소스패닝트리(상근이의 여행) - 2017/08/12 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	static int n;
+	static int m;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i=0; i<t; i++) {
+			n = sc.nextInt();
+			m = sc.nextInt();
+			
+			for(int j=0; j<m; j++) {
+				int start = sc.nextInt();
+				int end = sc.nextInt();
+			}
+			
+			System.out.println(n-1);
+			
+		}
+	}
+
+}
