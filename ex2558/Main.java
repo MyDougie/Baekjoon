f7ecc410b1fab1920e6e5fb215dd1e6dcb1ff127
ParkTaegyu <01046009100@hanmail.net>
2017-09-10 21:02:29 +0900

    입출력(A+B-2)

---
+++ ex2558/Main.java
@@ -0,0 +1,15 @@
+package ex2558;
+
+/**
+ * 입출력(A+B-2) - 2017/09/10 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Integer.parseInt(sc.nextLine()) + Integer.parseInt(sc.nextLine()));
+	}
+
+}
