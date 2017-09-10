99d0e2e01615fbdfdbf2b2bd307c1240a211ddf4
ParkTaegyu <01046009100@hanmail.net>
2017-09-10 22:21:55 +0900

    그대로 출력하기2

---
+++ ex11719/Main.java
@@ -0,0 +1,18 @@
+package ex11719;
+
+/**
+ * 그대로 출력하기2 - 2017/09/10 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+
+}
