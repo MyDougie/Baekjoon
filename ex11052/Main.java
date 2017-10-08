8827b3ae8cdd2e70ab1cbe2d0bb8fd8a22bd6b4c
ParkTaegyu <01046009100@hanmail.net>
2017-10-08 16:35:01 +0900

    DP(붕어빵 판매하기

--- ex11052/Main.java
+++ ex11052/Main.java
@@ -3,42 +3,28 @@ package ex11052;
 /**
  * DP(붕어빵 판매하기) - 2017/09/07 - Park Taegyu
  * */
+
 import java.util.Scanner;
 
 public class Main {
 
-	static int N;
-	static int p[];
-	static int dp[];
-	
-	void input() {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		p = new int [N+1];
-		dp = new int [N+1];
-		
-		for(int i=1; i<p.length; i++) {
-			p[i] = sc.nextInt();
-		}
-		
-	}
-	
-	void solve() {
-		dp[1] = p[1];
-		//dp[n] : 붕어빵 n개를 팔 떄 얻을 수 있는 최대 수익
-		for(int i=2; i<=N; i++) {
-			for(int j=1; j<=i; j++) {
-				dp[i] = Math.max(dp[i], dp[i-j] + p[j]);
-			}
-		}
-		
-		System.out.println(dp[N]);
-	}
-	
-	public static void main(String[] args) {
-		Main main = new Main();
-		main.input();
-		main.solve();
-	}
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      
+      int[] dp = new int[n+1];
+      int[] p = new int[n+1];
+      for(int i=1; i<=n; i++) {
+         p[i] = sc.nextInt();
+      }
+      
+      for(int i=1; i<=n; i++) {
+         for(int j=1; j<=i; j++) {
+            dp[i] = Math.max(dp[i], dp[i-j] + p[j]);
+         }
+      }
+      System.out.println(dp[n]);
+      
+   }
 
 }
