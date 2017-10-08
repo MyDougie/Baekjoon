bda69fa747f09d8324735b7175e5f78f8053f171
ParkTaegyu <01046009100@hanmail.net>
2017-10-08 18:52:01 +0900

    DP(LIS)

--- ex11055/Main.java
+++ ex11055/Main.java
@@ -11,28 +11,23 @@ public class Main {
 		int n = sc.nextInt();
 		int arr [] = new int[n+1];
 		int dp [] = new int[n+1];
-		int max = 0;
+		int ans = 0;
 		
 		for(int i=1; i<=n; i++){
 			arr[i] = sc.nextInt();
 		}
 		
-		for(int i=1; i<=n; i++){
-			int min = 0;
-			for(int j=0; j<i; j++){
-				if(arr[i] > arr[j]){
-					if(min < dp[j]){
-						min = dp[j];
-					}
+		for(int i=1; i<=n; i++) {
+			dp[i] = arr[i];
+			for(int j=0; j<i; j++) {
+				if(arr[j] < arr[i] && dp[i] < dp[j] + arr[i]) {
+					dp[i] = dp[j] + arr[i];
 				}
 			}
-			dp[i] = min + arr[i];
-			if(max < dp[i]){
-				max = dp[i];
-			}
+			ans = Math.max(ans, dp[i]);
 		}
 		
-		System.out.println(max);
+		System.out.println(ans);
 	
 	}
 
