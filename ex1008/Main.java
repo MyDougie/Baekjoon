a6ddb8138048c6333ac0822696286799f9f2ca5a
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 18:54:49 +0900

    A/B

---
+++ ex1008/Main.java
@@ -0,0 +1,15 @@
+package ex1008;
+
+/**
+ * A/B - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((double) sc.nextInt()/sc.nextInt());
+	}
+
+}
