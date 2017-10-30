0da851ef870387f18f3ebb2d583863df3e027b31
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 19:09:20 +0900

    기찍N

---
+++ ex2742/Main.java
@@ -0,0 +1,18 @@
+package ex2742;
+
+/**
+ * 기찍N - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		for(int i=x; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
