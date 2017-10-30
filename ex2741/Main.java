091d4c0b4cc77957e646474ff64d6133ac050129
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 19:09:12 +0900

    N찍기

---
+++ ex2741/Main.java
@@ -0,0 +1,18 @@
+package ex2741;
+
+/**
+ * N찍기 - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		for(int i=1; i<=x; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
