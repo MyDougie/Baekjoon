d95e79548022d33d7e7157533f328ee95e6216b8
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 18:47:44 +0900

    We Love kriii

---
+++ ex10718/Main.java
@@ -0,0 +1,13 @@
+package ex10718;
+
+/**
+ * We love kriii - 2017/10/30 - Park Taegyu
+ * */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
