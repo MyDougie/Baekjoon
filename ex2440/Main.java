8cc676c094e82ff741080683da5440b7b643ca08
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 19:25:59 +0900

    별 찍기-3

---
+++ ex2440/Main.java
@@ -0,0 +1,21 @@
+package ex2440;
+
+/**
+ * 별 찍기-3 - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
