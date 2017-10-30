234621abf13753429dcca726aa5f3b8494f93dba
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 18:57:44 +0900

    사칙연산

---
+++ ex10869/Main.java
@@ -0,0 +1,21 @@
+package ex10869;
+
+/**
+ * 사칙연산 - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
