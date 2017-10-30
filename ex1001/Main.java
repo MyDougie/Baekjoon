8c1344484575b866874c646373b0d5cf47cda257
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 18:39:30 +0900

    뺄셈

---
+++ ex1001/Main.java
@@ -0,0 +1,15 @@
+package ex1001;
+
+/**
+ * 뺄셈 - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() - sc.nextInt());
+	}
+
+}
