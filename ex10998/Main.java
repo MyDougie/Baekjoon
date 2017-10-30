4c714bbb05fe86ffeed24441accd822a9e7bb985
ParkTaegyu <01046009100@hanmail.net>
2017-10-30 18:55:04 +0900

    A*B

---
+++ ex10998/Main.java
@@ -0,0 +1,15 @@
+package ex10998;
+
+/**
+ * A*B - 2017/10/30 - Park Taegyu
+ * */
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * sc.nextInt());
+	}
+
+}
